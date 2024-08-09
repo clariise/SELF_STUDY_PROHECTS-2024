d48795f5ccfb91155448469f6b34d04d54423eab
clariise <clcarmona001@gmail.com>
2024-08-09 16:52:45 +0800

    Added other functionalities in JS

---
+++ Iphone Calculator/script.js
@@ -0,0 +1,21 @@
+const display = document.getElementById("display");
+
+//sign for negative and positive
+
+if (value === "-") {
+  if (display.value.startWith("-")) {
+    dis[ByteLengthQueuingStrategy.value];
+  }
+}
+
+function addToDisplay(input) {
+  display.value += input;
+}
+
+function calculate() {
+  display.value = eval(display.value);
+}
+
+function clearDisplay() {
+  document.getElementById("display").value = " ";
+}
