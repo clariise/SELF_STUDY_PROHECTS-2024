c779f4d7722ddcf71439bcb0ad867c838f67fa71
clariise <clcarmona001@gmail.com>
2024-08-09 18:06:23 +0800

    Added functionalities of each operators

--- Iphone Calculator/script.js
+++ Iphone Calculator/script.js
@@ -2,9 +2,11 @@ const display = document.getElementById("display");
 
 //sign for negative and positive
 
-if (value === "-") {
-  if (display.value.startWith("-")) {
-    dis[ByteLengthQueuingStrategy.value];
+function toggleSign() {
+  if (display.value.startsWith("-")) {
+    display.value = display.value.substring(1);
+  } else {
+    display.value = "-" + display.value;
   }
 }
 
@@ -18,4 +20,8 @@ function calculate() {
 
 function clearDisplay() {
   document.getElementById("display").value = " ";
+}
+
+function percent() {
+  display.value = parseFloat(display.value) / 100;
 }
