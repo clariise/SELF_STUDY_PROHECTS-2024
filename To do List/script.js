dd66a9379bf081c982517f0a710dc0f052a2c92c
clariise <clcarmona001@gmail.com>
2024-08-12 19:09:29 +0800

    added functionalities

--- To do List/script.js
+++ To do List/script.js
@@ -6,7 +6,17 @@
 //call function is a function pass an argument
 //funtion na no name called anonymous function
 // # id
-//
-document.querySelector("#addTask").addEventListener("click", function () {
-  alert("Button Clicked");
+// arrow function is a shorter way to write a function
+//function () can be () => { }
+
+document.querySelector("#addTask").addEventListener("click", () => {
+  if (document.querySelector("#taskInput").value != "") {
+    document.querySelector("#taskArea").innerHTML += `<div> 
+  <button class="btn btn-light">✅</button>
+        <span>${document.querySelector("#taskInput").value}</span>
+    </div> <hr>`;
+    document.querySelector("#taskInput").value = "";
+  } else {
+    alert("Field are empty");
+  }
 });
