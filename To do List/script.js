d370c5eb78b967957c47a5fb6da68889551d7a02
clariise <clcarmona001@gmail.com>
2024-08-13 08:56:26 +0800

    finished todolist

--- To do List/script.js
+++ To do List/script.js
@@ -9,14 +9,33 @@
 // arrow function is a shorter way to write a function
 //function () can be () => { }
 
+//1.  "addTask" dapat naclick using eventlistener --always check using alert
+//2.  "taskInput"--once na makapag type then clicked button maappear dapat yung tnype mo
+//3.   append all the text you type on "taskArea" by using back tick ` and div you created + $text
+//4.
+
 document.querySelector("#addTask").addEventListener("click", () => {
   if (document.querySelector("#taskInput").value != "") {
     document.querySelector("#taskArea").innerHTML += `<div> 
-  <button class="btn btn-light">✅</button>
+  <button class="btn btn-light markButton">✅</button>
         <span>${document.querySelector("#taskInput").value}</span>
     </div> <hr>`;
     document.querySelector("#taskInput").value = "";
   } else {
     alert("Field are empty");
   }
+
+  //nothing events pa dito
+  const markButtons = document.querySelectorAll(".markButton");
+  markButtons.forEach((markButton) => {
+    //here is the event
+    markButton.addEventListener("click", (event) => {
+      event.target.parentElement.lastElementChild.style =
+        "text-decoration: line-through; font-weight: 900";
+      event.target.remove();
+    });
+
+    //paano madetermine anong line and pinindot
+    //target event property-- use para makuha nya element kung saan nanagyare yung event
+  });
 });
